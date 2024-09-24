d73f7d6347ebbf3a5c53f4e64ac822164d4f5f42
WEBAN <zkzk1231@gmail.com>
2024-09-24 15:12:01 +0900

    11653 문제 해결
    개선점 :
    1. 명확한 종료 조건: for 루프를 사용하여 종료 조건이 명확하기 때문에 구조가 더 직관적이고 이해하기 쉽다.
    2. 불필요한 반복 감소: 순차적으로 검사하여 불필요한 반복을 줄일 수 있다.

--- BAEKJOON/Bronze1/11653.cpp
+++ BAEKJOON/Bronze1/11653.cpp
@@ -11,21 +11,26 @@ using namespace std;
 
 int main()
 {
-    int N = 0, d = 2;
-    cin >> N;
+    // cin, cout은 기본적으로 C의 stdio.h와 동기화되어있다.
+    // stdio.h의 버퍼와 iostream의 버퍼를 같이 쓴다는 의미.
+    // 동기화를 유지하는 작업이 추가되다 보니 상대적으로 속도가 느리다.
+    // 아래의 함수로 동기화를 해제해 줄 수 있다.
+    // 동기화를 해제하게 되면 아래와 같은 사항을 유의해야 한다.
+    // C의 입출력 방식과 동시 사용 불가 (printf, scanf, getchar, puts, gets)
+    // -> 동기화를 해제했기 때문에 C의 버퍼와 C++의 버퍼가 독립적이다. 따라서 출력되는 순서를 보장할 수 없다.
+    // 멀티쓰레드 불가 - 싱글쓰레드 환경에서만 사용(실무에서 사용 불가)
+    ios::sync_with_stdio(false);
 
-    while(true)
-    {
-        if(N % d == 0)
-        {
-            N = N / d;
-            cout << d << endl;
-            if(N == 1)
-            {
-                break;
-            }
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N = 0;
+    cin >> N;
+    for (int i = 2; i <= N; i++) {
+        while (N % i == 0) {
+            //endl보다 "\n"이 빠름
+            cout << i << "\n";
+            N /= i;
         }
-        else
-            d++;
     }
 }
