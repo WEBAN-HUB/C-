9c9281c691d6170cdce4a17bbe2e39d8af3736cc
WEBAN <zkzk1231@gmail.com>
2024-09-24 13:34:22 +0900

    11653 문제 시간 초과

---
+++ BAEKJOON/Bronze1/11653.cpp
@@ -0,0 +1,31 @@
+// 문제
+// 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+
+// 출력
+// N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 0, d = 2;
+    cin >> N;
+
+    while(true)
+    {
+        if(N % d == 0)
+        {
+            N = N / d;
+            cout << d << endl;
+            if(N == 1)
+            {
+                break;
+            }
+        }
+        else
+            d++;
+    }
+}
