26128761d5987e83e574662bb80c491b07923838
WEBAN <zkzk1231@gmail.com>
2024-09-25 14:09:26 +0900

    1085 문제 해결

---
+++ BAEKJOON/Bronze3/1085.cpp
@@ -0,0 +1,39 @@
+// 문제
+// 한수는 지금 (x, y)에 있다. 직사각형은 각 변이 좌표축에 평행하고, 왼쪽 아래 꼭짓점은 (0, 0), 오른쪽 위 꼭짓점은 (w, h)에 있다. 직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 x, y, w, h가 주어진다.
+
+// 출력
+// 첫째 줄에 문제의 정답을 출력한다.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, w, h,min=0;
+
+    cin >> x >> y >> w >> h;
+
+    if(w - x < x)
+    {
+        min = w -x;
+    }
+    else
+    {
+        min = x;
+    }
+
+
+    if( h - y < y && h - y < min)
+    {
+        min = h - y;
+    }
+    else
+    {
+        if(y < min)
+            min = y;
+    }
+
+    cout << min;
+}
