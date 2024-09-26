bde206e3545362ba00a6baf85ad7e255efcd51be
WEBAN <zkzk1231@gmail.com>
2024-09-27 00:13:53 +0900

    3009 ^= 연산을 활용한 문제 해결 방법

--- BAEKJOON/Bronze3/3009.cpp
+++ BAEKJOON/Bronze3/3009.cpp
@@ -6,37 +6,17 @@
 
 // 출력
 // 직사각형의 네 번째 점의 좌표를 출력한다.
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
-    int count = 3, x , y;
-    vector<int> arrX, arrY;
+    // 아래와 같은 원리로 문제를 해결할 수 있다.
+    // 초기 값에 계산할 값 ^=연산(비트 XOR 연산)을 수행하게 되면 
+    // 초기 값과 게산할 값의 다른 비트 부분들이 1로 변한 어떠한 값으로 변하게 된다.
+    // 그런데 변화된 어떠한 값에 계산했던 같은 값으로 다시 ^= 연산을 수행하게 되면
+    // 초기 값으로 다시 돌아가게 된다.
 
-    while(count > 0)
-    {
-        cin >> x >> y;
-        if(find(arrX.begin(),arrX.end(),x) == arrX.end())
-        {
-            arrX.push_back(x);
-        }
-        else
-        {
-            arrX.erase(find(arrX.begin(),arrX.end(),x));
-        }
-        if(find(arrY.begin(),arrY.end(),y) == arrY.end())
-        {
-            arrY.push_back(y);
-        }
-        else
-        {
-            arrY.erase(find(arrY.begin(),arrY.end(),y));
-        }
-        count--;
-    }
-    cout << arrX.front() << " " << arrY.front();
-
+   int a,b,c,d,e,f;
+   scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+   printf("%d %d",a^c^e,b^d^f);
 }
